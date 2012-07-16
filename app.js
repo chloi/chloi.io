b3e328e726ef21ff45dc4be39d9f5323932d4276
Rob Ellis <rob@chloi.io>
2012-07-16 14:27:56 -0700

    Added blog engine to website

--- app.js
+++ app.js
@@ -40,8 +40,11 @@ app.configure(function(){
 // index
 // --------------------
 
-app.get("/", function(req, rsp){      
-  rsp.render("index")
+app.get("/",  m.info, m.published, function(req, rsp){      
+  rsp.render("index",{
+    info: req.info,
+    articles: req.published
+  })
 })
 
 // --------------------
@@ -57,6 +60,64 @@ app.post("/messages", function(req, rsp){
     }
   })
 })
+
+
+// --------------------
+// articles / blog
+// --------------------
+
+app.get("/blog/articles", m.info, m.published, function(req, rsp){
+  rsp.render("blog/articles", {
+    info: req.info,
+    articles: req.published
+  })
+})
+
+// Same as above
+app.get("/blog", m.info, m.published, function(req, rsp){
+  rsp.render("blog/articles", {
+    info: req.info,
+    articles: req.published
+  })
+})
+
+
+app.get("/blog/articles.atom", m.info, m.published, m.feed, function(req, rsp){
+  rsp.render("blog/atom", {
+    layout: false,
+    info: req.info,
+    articles: req.published
+  })
+})
+
+app.get("/blog/unpublished/:slug", m.info, m.published, function(req, rsp){
+  if(process.env.NODE_ENV == "production"){
+    rsp.send("Nothing to see here.", 404)
+  }else{
+    rsp.render("blog/show", {
+      info: req.info,
+      article: { slug: req.params.slug, date: "Unpublished" }
+    })    
+  }
+})
+
+app.get("/blog/:slug", m.info, m.published, function(req, rsp){
+  // check for published article
+  var article;
+  req.published.forEach(function(a){
+    if(a.slug === req.params.slug)
+      article = a;
+  })
+  if(article){
+    rsp.render("blog/show", {
+      info: req.info,
+      article: article
+    })
+  }else{
+    rsp.send("Nothing to see here.", 404)
+  }
+})
+
 
 // --------------------
 // export
